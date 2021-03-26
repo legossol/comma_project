efa846b9dd803041037376182b14d81e819df5d9
legossol <wkdgothf@gmail.com>
2021-03-26 09:07:22 +0900

    repo CRUD

--- be/demo/src/main/java/com/example/demo/uss/repository/UserRepository.java
+++ be/demo/src/main/java/com/example/demo/uss/repository/UserRepository.java
@@ -1,34 +1,40 @@
 package com.example.demo.uss.repository;
 
 
-import java.util.List;
+import java.util.*;
 
 import com.example.demo.uss.domain.User;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
-    @Query("select * from users ")
-    User findByUserByGender(String gender);
 
     @Modifying
-    @Query("update user u set u. = :status where u.uerNo = :userNo")
-    int updateUserSetStatusForName(@Param("status") Integer status, 
-      @Param("name") String name);
+    @Query(value = "insert into users (name, age, email, phoneNumber, phone_number) values (:name, :age :email, :phoneNumber, :phone_number)", nativeQuery = true)
+    void insertUser(@Param("name") String name, @Param("age") String age, 
+    @Param("email") String email, @Param("phoneNumber") String phoneNumber, @Param("phone_number") String phone_number);
+    //Create
 
+    @Query("select * from User u")
+    List<User> findAllUsers(Sort sort);
+    
+    //Read
     @Modifying
     @Query(value = "update users u set  where u.name = ? u.age = ? u.email = ? u.phoneNumber = ?", 
         nativeQuery = true)
-      int updateUserSetStatusForNameNative(Integer name, String age, String email, String phoneNumber);    
-
-
-
+    int updateUserSetStatusForNameNative(Integer name, String age, String email, String phoneNumber);    
+    
+      //update
     @Modifying
-    @Query(value = "insert into users (name, age, email, phoneNumber) values (:name, :age :email, :phoneNumber)", nativeQuery = true)
-    void insertUser(@Param("name") String name, @Param("age") String age, 
-    @Param("email") String email, @Param("phoneNumber") String phoneNumber);
+    @Query("delete u from users u where u.name=:name)
+    void deleteUser(@Param("name")String name);
+    //delete
 
+
+
+    
     //@Query(value="SELECT * FROM USER", nativeQuery=true)
 }
